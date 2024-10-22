e952d247ef5ad875f92ef48730e7a8ca9d65bc2f
dody <jozino@gmail.com>
2024-10-22 23:21:22 +0200

    compile compatible with linux

--- compile_proto.bat
+++ compile_proto.bat
@@ -1,3 +1,1 @@
-del "my_service_pb2.py"
-del "my_service_pb2_grpc.py"
-python.exe -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. my_service.proto
+python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. my_service.proto
